9310df1bab62eb7909c16e41e1dd63501e329e04
NurAnnisa01 <136819466+NurAnnisa01@users.noreply.github.com>
2024-08-10 16:05:26 +0000

    chapter 1 dan 2

---
+++ cp1.py
@@ -0,0 +1,10 @@
+import pygame # type: ignore
+from PIL import Image
+import cv2 # type: ignore
+import pydub # type: ignore
+import tkinter as tk
+
+import moviepy.editor as mp # type: ignore
+
+# Konfirmasi instalasi
+print("✅ Semua pustaka telah terinstal dengan benar!")
